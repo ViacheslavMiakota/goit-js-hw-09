6c94f59f34f9d624a8122683b223782e590de861
ViacheslavMiakota <parazyt2010@gmail.com>
2022-10-06 21:02:02 +0300

    add corect

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-  delayName: document.querySelector("input[name='delay']"),
-  stepName: document.querySelector("input[name='step']"),
-  amountName: document.querySelector("input[name='amount']"),
   submitBtn: document.querySelector('form'),
 };
-console.log(refs.delayName);
-console.log(refs.stepName);
-console.log(refs.amountName);
 console.log(refs.submitBtn);
 
 function createPromise(position, delay) {
@@ -20,35 +14,50 @@ function createPromise(position, delay) {
       } else {
         reject({ position, delay });
       }
-    });
-    refs.delayName;
+    }, delay);
   });
 }
-refs.submitBtn.addEventListener('submit', () => {
-  let numPosition = 0;
-  for (let i = 0; i < refs.amountName.value; i += 1) {
-    numPosition += 1;
-    console.log(numPosition);
-    const dalayTotal = refs.delayName.value + refs.stepName.value;
 
-    createPromise(numPosition, dalayTotal)
+refs.submitBtn.addEventListener('submit', event => {
+  event.preventDefault();
+  let numPosition = 0;
+  const delayTotal =
+    Number(event.currentTarget.delay.value) +
+    Number(event.currentTarget.step.value);
+  for (let i = 0; i < Number(event.currentTarget.amount.value); i++) {
+    createPromise(numPosition, delayTotal)
       .then(({ position, delay }) => {
-        Notify.success('✅ Fulfilled promise ${position} in ${delay}ms', {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
           position: 'center-center',
           backOverlay: true,
           clickToClose: true,
         });
       })
       .catch(({ position, delay }) => {
-        Notify.failure('❌ Rejected promise ${position} in ${delay}ms', {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
           position: 'center-center',
           backOverlay: true,
           clickToClose: true,
         });
       });
+    numPosition += event.currentTarget.step.value;
   }
-  event.preventDefault();
 });
 function preventDefault(event) {
   event.preventDefault();
 }
+
+// let delay = 1000;
+// const step = 500;
+// const amount = 5;
+
+// for (let i = 0; i < amount; i++) {
+//   createPromise(i + 1, delay)
+//     .then(({ position, delay }) => {
+//       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//     })
+//     .catch(({ position, delay }) => {
+//       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//     });
+//   delay += event.currentTarget.step.value;
+// }
